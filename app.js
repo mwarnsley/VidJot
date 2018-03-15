4db73888de74bd5ece30561c5ffabba527797857
Marcus <marcus.warnsley@gmail.com>
2018-03-15 11:56:29 -0400

    Setting up the nodemon piece along with setting up some of the middleware

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = 4000;
+
+// Including the express middleware
+app.use((req, res, next) => {
+  console.log(Date.now());
+  next();
+});
+
+// Setup the index route
+app.get('/', (req, res) => {
+  res.send('INDEX');
+});
+
+app.get('/about', (req, res) => {
+  res.send('ABOUT');
+});
 
 app.listen(PORT, () => {
   console.log(`Application running on port: ${PORT}`);
