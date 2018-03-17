4daee9d26e09cd90ad4c8e4028c147617cbf2916
Marcus <marcus.warnsley@gmail.com>
2018-03-17 16:01:12 -0400

    Adding in the partial for the errors for it to show on every page along with adding in the registration saving routes in the routes files

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,26 @@ router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
+// Post route for register form
+router.post('/register', (req, res) => {
+  let errors = [];
+  if (req.body.password !== req.body.password2) {
+    errors.push({text: 'Password do not match!'});
+  }
+  if (req.body.password.length < 4) {
+    errors.push({text: 'Password must be at least 4 characters'});
+  }
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send('PASSED');
+  }
+});
+
 module.exports = router;
