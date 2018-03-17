6291388ee94674246362c84f9081900a22634cd3
Marcus <marcus.warnsley@gmail.com>
2018-03-16 23:39:01 -0400

    Adding in sessions and adding in connect flash to show messages for the crud operations

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -24,6 +26,26 @@ app.use(bodyParser.json());
 
 // override with the X-HTTP-Method-Override header in the request
 app.use(methodOverride('_method'));
+
+// Middelware for express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Middleware for using connect-flash
+app.use(flash());
+
+// Setting Global Variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Including the handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -84,7 +106,10 @@ app.post('/ideas', (req, res) => {
     };
     new Idea(newUser)
       .save()
-      .then(idea => res.redirect('/ideas'))
+      .then(idea => {
+        req.flash('success_msg', 'Video Idea Added');
+        res.redirect('/ideas');
+      })
       .catch(error => console.log('Error Saving: ', error));
   }
 });
@@ -97,7 +122,13 @@ app.put('/ideas/:id', (req, res) => {
       idea.title = req.body.title;
       idea.details = req.body.details;
 
-      idea.save().then(idea => res.redirect('/ideas').catch(error => console.log('Error saving idea: ', error)));
+      idea
+        .save()
+        .then(idea => {
+          req.flash('success_msg', 'Video Idea Updated');
+          res.redirect('/ideas');
+        })
+        .catch(error => console.log('Error saving idea: ', error));
     })
     .catch(error => console.log('Error Updating Idea: ', error));
 });
@@ -105,7 +136,10 @@ app.put('/ideas/:id', (req, res) => {
 // Delet Idea
 app.delete('/ideas/:id', (req, res) => {
   Idea.remove({_id: req.params.id})
-    .then(() => res.redirect('/ideas'))
+    .then(() => {
+      req.flash('success_msg', 'Video Idea Removed');
+      res.redirect('/ideas');
+    })
     .catch(error => console.log('Error Deleting Idea: ', error));
 });
 
