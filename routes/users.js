30d0784f2d31766af4d9d49586921ac48e73be97
Marcus <marcus.warnsley@gmail.com>
2018-03-18 00:21:41 -0400

    Editing the ability to logout of the application

--- routes/users.js
+++ routes/users.js
@@ -77,4 +77,11 @@ router.post('/register', (req, res) => {
   }
 });
 
+// Log the user out
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
+
 module.exports = router;
